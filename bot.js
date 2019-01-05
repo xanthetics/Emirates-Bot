df3928aaad90c5e60c61715a3df234ca885be9ef
xanthetics <46301622+xanthetics@users.noreply.github.com>
2019-01-05 11:51:22 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NTI5ODg3ODQyMzk0OTYzOTg5.DxKMqw.a2WuCTNE1XRPhzsa78_XdPadPdA);
